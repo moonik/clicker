816ec06aff230cb80fdb3b50a3291b7a1b7d64db
Roman <romanveos@gmail.com>
2017-04-11 01:26:14 +0300

    added posibility to buy factories and grandmothers to get more cookies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 var cookies = 0;
 
+var factoryPrice= 700;
+var factoryCookies = 10;
+var amountFactories = 0;
+
+var grandmaPrice = 500;
+var grandmaCookies = 2;
+var amountGrandma = 0;
+
+var ifGrandma = false;
+var ifFactory = false;
+
 $( document ).ready(function() {
   var ifStarted = false;
 
@@ -7,6 +18,7 @@ $( document ).ready(function() {
   {
     $("#start").css('display', 'block');
     $("#cookie").css('display', 'none');
+    $("#shop").css('display', 'none');
   }
 });
 
@@ -15,13 +27,53 @@ function start()
   ifStarted = true;
   if(ifStarted){
     $("#cookie").css('display', 'block');
+    $("#shop").css('display', 'block');
     $("#start").css('display', 'none');
   }
-
-}
+};
 
 function cookieClick()
 {
   cookies++;
   $('.cookies').html("Your cookies: " + cookies);
-}
+};
+
+function buyFactory(){
+  if(cookies >= factoryPrice){
+    amountFactories++;
+    cookies -= factoryPrice;
+    if(!ifFactory){
+        factoriesCookies();
+    }
+  }else {
+    console.log("You don't have enought cookies");
+  }
+};
+
+function buyGrandma(){
+  if(cookies >= grandmaPrice){
+    amountGrandma++;
+    cookies -= grandmaPrice;
+    if(!ifGrandma){
+        grandmaCookie();
+    }
+  }else {
+    console.log("You don't have enought cookies");
+  }
+};
+
+function factoriesCookies(){
+  ifFactory = true;
+  setInterval(function(){
+      cookies += factoryCookies*amountFactories;
+    $('.cookies').html("Your cookies: " + cookies);
+  }, 1000);
+};
+
+function grandmaCookie(){
+  ifGrandma = true;
+  setInterval(function(){
+      cookies += grandmaCookies*amountGrandma;
+    $('.cookies').html("Your cookies: " + cookies);
+  }, 1000);
+};
