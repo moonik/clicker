35311f5d55c3b4084d821c070f2ed74fb36f8fed
Roman <romanveos@gmail.com>
2017-04-17 01:20:37 +0300

    added animation coockie when clicked

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var cookies = 0;
+var box;
+var t;
+var ifInterval = false;
+var ifFinterval = false;
+var ifGinterval = false;
+var posF = 0;
+var posG = 0;
 
-var factoryPrice= 700;
+var factoryPrice= 1;
 var factoryCookies = 10;
 var amountFactories = 0;
 
-var grandmaPrice = 500;
+var grandmaPrice = 100;
 var grandmaCookies = 2;
 var amountGrandma = 0;
 
@@ -19,6 +26,9 @@ $( document ).ready(function() {
     $("#start").css('display', 'block');
     $("#cookie").css('display', 'none');
     $("#shop").css('display', 'none');
+    $("#cook").css('display', 'none');
+    $("#cookF").css('display', 'none');
+    $("#cookG").css('display', 'none');
   }
 });
 
@@ -26,6 +36,7 @@ function start()
 {
   ifStarted = true;
   if(ifStarted){
+    box = document.getElementById("cook");
     $("#cookie").css('display', 'block');
     $("#shop").css('display', 'block');
     $("#start").css('display', 'none');
@@ -34,12 +45,34 @@ function start()
 
 function cookieClick()
 {
+  if(!ifInterval){
+    var t = setInterval(move, 1);
+  }else{
+    clearInterval(t);
+    pos = 0;
+  }
   cookies++;
   $('.cookies').html("Your cookies: " + cookies);
 };
 
+// starting position
+var pos = 0;
+
+function move() {
+  ifInterval = true;
+  if(pos == 300){
+    clearInterval(t);
+    $("#cook").css('display', 'none');
+  }else{
+    $("#cook").css('display', 'block');
+    pos += 1;
+    box.style.top = -pos+"px";
+  }
+}
+
 function buyFactory(){
   if(cookies >= factoryPrice){
+    $("#cookF").css('display', 'block');
     amountFactories++;
     cookies -= factoryPrice;
     if(!ifFactory){
@@ -52,6 +85,7 @@ function buyFactory(){
 
 function buyGrandma(){
   if(cookies >= grandmaPrice){
+    $("#cookG").css('display', 'block');
     amountGrandma++;
     cookies -= grandmaPrice;
     if(!ifGrandma){
@@ -63,7 +97,6 @@ function buyGrandma(){
 };
 
 function factoriesCookies(){
-  ifFactory = true;
   setInterval(function(){
       cookies += factoryCookies*amountFactories;
     $('.cookies').html("Your cookies: " + cookies);
